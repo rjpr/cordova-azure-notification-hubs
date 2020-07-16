c895a364243e57ee17eded78b87aa803fc8df6e2
Martin Fracker, Jr <martin.frackerjr@gmail.com>
2020-07-16 12:27:08 -0500

    Fix errors and deprecation warnings

--- src/android/com/adobe/phonegap/push/PushInstanceIDListenerService.java
+++ src/android/com/adobe/phonegap/push/PushInstanceIDListenerService.java
@@ -6,20 +6,20 @@ import android.content.SharedPreferences;
 import android.util.Log;
 
 import com.google.firebase.iid.FirebaseInstanceId;
-import com.google.firebase.iid.FirebaseInstanceIdService;
+import com.google.firebase.iid.FirebaseInstanceIdReceiver;
+import com.google.firebase.messaging.FirebaseMessagingService;
 
 import org.json.JSONException;
 
 import java.io.IOException;
 
-public class PushInstanceIDListenerService extends FirebaseInstanceIdService implements PushConstants {
+public class PushInstanceIDListenerService extends FirebaseMessagingService implements PushConstants {
     public static final String LOG_TAG = "Push_InsIdService";
 
     @Override
-    public void onTokenRefresh() {
+    public void onNewToken(String token) {
         // Get updated InstanceID token.
-        String refreshedToken = FirebaseInstanceId.getInstance().getToken();
-        Log.d(LOG_TAG, "Refreshed token: " + refreshedToken);
+        Log.d(LOG_TAG, "Refreshed token: " + token);
         // TODO: Implement this method to send any registration to your app's servers.
         //sendRegistrationToServer(refreshedToken);
     }
