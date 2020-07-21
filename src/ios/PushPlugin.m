069ce4736505e21fb334cbf6d0d26bbac5384a2e
Martin Fracker, Jr <martin.frackerjr@gmail.com>
2020-07-21 09:09:33 -0500

    Make compatible with in-house project; update messaging framework

--- src/ios/PushPlugin.m
+++ src/ios/PushPlugin.m
@@ -27,6 +27,9 @@
 
 #import <WindowsAzureMessaging/SBNotificationHub.h>
 
+
+#import <objc/runtime.h>
+
 @implementation PushPlugin : CDVPlugin
 
 @synthesize notificationMessage;
@@ -41,7 +44,7 @@
 
 - (void)unregister:(CDVInvokedUrlCommand*)command;
 {
-    SBNotificationHub* hub = [[SBNotificationHub alloc] initWithConnectionString:self.connectionString 
+    SBNotificationHub* hub = [[SBNotificationHub alloc] initWithConnectionString:self.connectionString
                                                              notificationHubPath:self.notificationHubPath];
 
     [hub unregisterNativeWithCompletion:^(NSError* error) {
@@ -57,6 +60,7 @@
 
 - (void)init:(CDVInvokedUrlCommand*)command;
 {
+    [self initPlugin];
     [self.commandDelegate runInBackground:^ {
 
         NSLog(@"Push Plugin register called");
@@ -200,7 +204,7 @@
          (UIRemoteNotificationTypeBadge | UIRemoteNotificationTypeSound | UIRemoteNotificationTypeAlert)];
 #endif
 
-        if (notificationMessage) {			// if there is a pending startup notification
+        if (notificationMessage) {            // if there is a pending startup notification
             dispatch_async(dispatch_get_main_queue(), ^{
                 // delay to allow JS event handlers to be setup
                 [self performSelector:@selector(notificationReceived) withObject:nil afterDelay: 0.5];
@@ -233,14 +237,15 @@
     return myAction;
 }
 
-- (void)didRegisterForRemoteNotificationsWithDeviceToken:(NSData *)deviceToken {
+- (void)didRegisterForRemoteNotificationsWithDeviceToken:(NSNotification *)notification {
+    NSData *deviceToken = [notification object];
     if (self.callbackId == nil) {
         NSLog(@"Unexpected call to didRegisterForRemoteNotificationsWithDeviceToken, ignoring: %@", deviceToken);
         return;
     }
     NSLog(@"Push Plugin register success: %@", deviceToken);
 
-    SBNotificationHub* hub = [[SBNotificationHub alloc] initWithConnectionString:self.connectionString 
+    SBNotificationHub* hub = [[SBNotificationHub alloc] initWithConnectionString:self.connectionString
                                                              notificationHubPath:self.notificationHubPath];
 
     [hub registerNativeWithDeviceToken:deviceToken tags:nil completion:^(NSError* error) {
@@ -508,6 +513,202 @@
             completionHandler = nil;
         }
     }
+}
+
+
+
+
+
+static char launchNotificationKey;
+static char coldstartKey;
+
+- (void)initPlugin
+{
+    [[NSNotificationCenter defaultCenter] addObserver:self
+                                             selector:@selector(createNotificationChecker:)
+                                                 name:UIApplicationDidFinishLaunchingNotification
+                                               object:nil];
+    [[NSNotificationCenter defaultCenter]addObserver:self
+                                            selector:@selector(pushPluginOnApplicationDidBecomeActive:)
+                                                name:UIApplicationDidBecomeActiveNotification
+                                              object:nil];
+    [[NSNotificationCenter defaultCenter]addObserver:self
+                                            selector:@selector(didRegisterForRemoteNotificationsWithDeviceToken:)
+                                                name:@"CAPDidRegisterForRemoteNotificationsWithDeviceToken"
+                                              object:nil];
+    [[NSNotificationCenter defaultCenter]addObserver:self
+                                            selector:@selector(didFailToRegisterForRemoteNotificationsWithError:)
+                                                name:@"CAPDidFailToRegisterForRemoteNotificationsWithDeviceToken"
+                                              object:nil];
+    [[NSNotificationCenter defaultCenter]addObserver:self
+                                            selector:@selector(didReceiveRemoteNotification:)
+                                                name:@"didReceiveRemoteNotificationWithOnlyUserInfo"
+                                              object:nil];
+    [[NSNotificationCenter defaultCenter]addObserver:self
+                                            selector:@selector(didReceiveRemoteNotificationWithCompletionHandler:)
+                                                name:@"didReceiveRemoteNotificationWithCompletionHandler"
+                                              object:nil];
+}
+
+// This code will be called immediately after application:didFinishLaunchingWithOptions:. We need
+// to process notifications in cold-start situations
+- (void)createNotificationChecker:(NSNotification *)notification
+{
+    NSLog(@"createNotificationChecker");
+    if (notification)
+    {
+        NSDictionary *launchOptions = [notification userInfo];
+        if (launchOptions) {
+            NSLog(@"coldstart");
+            self.launchNotification = [launchOptions objectForKey: @"UIApplicationLaunchOptionsRemoteNotificationKey"];
+            self.coldstart = [NSNumber numberWithBool:YES];
+        } else {
+            NSLog(@"not coldstart");
+            self.coldstart = [NSNumber numberWithBool:NO];
+        }
+    }
+}
+
+- (void) didReceiveRemoteNotification:(NSNotification *)notification {
+    NSLog(@"clicked on the shade");
+    PushPlugin *pushHandler = self;
+    pushHandler.notificationMessage = [notification object];
+    pushHandler.isInline = NO;
+    [pushHandler notificationReceived];
+}
+
+typedef void(^UIBackgroundFetchResultBlock)(UIBackgroundFetchResult);
+
+- (void)didReceiveRemoteNotificationWithCompletionHandler:(NSNotification *) notification {
+    NSLog(@"didReceiveNotification with fetchCompletionHandler");
+    NSDictionary* dict = [notification object];
+    UIApplication *application = dict[@"application"];
+    NSDictionary *userInfo = dict[@"userInfo"];
+    UIBackgroundFetchResultBlock completionHandler = dict[@"completionHandler"];
+    
+    self.notificationMessage = userInfo;
+    self.isInline = NO;
+
+    // app is in the foreground so call notification callback
+    if (application.applicationState == UIApplicationStateActive) {
+        NSLog(@"app active");
+        self.notificationMessage = userInfo;
+        self.isInline = YES;
+        [self notificationReceived];
+
+        completionHandler(UIBackgroundFetchResultNewData);
+    }
+    // app is in background or in stand by
+    else {
+        NSLog(@"app in-active");
+
+        // do some convoluted logic to find out if this should be a silent push.
+        long silent = 0;
+        id aps = [userInfo objectForKey:@"aps"];
+        id contentAvailable = [aps objectForKey:@"content-available"];
+        if ([contentAvailable isKindOfClass:[NSString class]] && [contentAvailable isEqualToString:@"1"]) {
+            silent = 1;
+        } else if ([contentAvailable isKindOfClass:[NSNumber class]]) {
+            silent = [contentAvailable integerValue];
+        }
+
+        if (silent == 1) {
+            NSLog(@"this should be a silent push");
+            void (^safeHandler)(UIBackgroundFetchResult) = ^(UIBackgroundFetchResult result){
+                dispatch_async(dispatch_get_main_queue(), ^{
+                    completionHandler(result);
+                });
+            };
+
+            if (self.handlerObj == nil) {
+                self.handlerObj = [NSMutableDictionary dictionaryWithCapacity:2];
+            }
+
+            id notId = [userInfo objectForKey:@"notId"];
+            if (notId != nil) {
+                NSLog(@"Push Plugin notId %@", notId);
+                [self.handlerObj setObject:safeHandler forKey:notId];
+            } else {
+                NSLog(@"Push Plugin notId handler");
+                [self.handlerObj setObject:safeHandler forKey:@"handler"];
+            }
+
+            self.notificationMessage = userInfo;
+            self.isInline = NO;
+            [self notificationReceived];
+        } else {
+            NSLog(@"just put it in the shade");
+            //save it for later
+            self.launchNotification = userInfo;
+
+            completionHandler(UIBackgroundFetchResultNewData);
+        }
+    }
+}
+
+- (BOOL)userHasRemoteNotificationsEnabled {
+    UIApplication *application = [UIApplication sharedApplication];
+    if ([[UIApplication sharedApplication] respondsToSelector:@selector(registerUserNotificationSettings:)]) {
+        return application.currentUserNotificationSettings.types != UIUserNotificationTypeNone;
+    } else {
+#pragma GCC diagnostic push
+#pragma GCC diagnostic ignored "-Wdeprecated-declarations"
+        return application.enabledRemoteNotificationTypes != UIRemoteNotificationTypeNone;
+#pragma GCC diagnostic pop
+    }
+}
+
+- (void)pushPluginOnApplicationDidBecomeActive:(NSNotification *)notification {
+
+    NSLog(@"active");
+
+    UIApplication *application = notification.object;
+
+    PushPlugin *pushHandler = self;
+    if (pushHandler.clearBadge) {
+        NSLog(@"PushPlugin clearing badge");
+        //zero badge
+        application.applicationIconBadgeNumber = 0;
+    } else {
+        NSLog(@"PushPlugin skip clear badge");
+    }
+
+    if (self.launchNotification) {
+        pushHandler.isInline = NO;
+        pushHandler.coldstart = self.coldstart;
+        pushHandler.notificationMessage = self.launchNotification;
+        self.launchNotification = nil;
+        self.coldstart = [NSNumber numberWithBool:NO];
+        [pushHandler performSelectorOnMainThread:@selector(notificationReceived) withObject:pushHandler waitUntilDone:NO];
+    }
+}
+
+// The accessors use an Associative Reference since you can't define a iVar in a category
+// http://developer.apple.com/library/ios/#documentation/cocoa/conceptual/objectivec/Chapters/ocAssociativeReferences.html
+- (NSMutableArray *)launchNotification
+{
+    return objc_getAssociatedObject(self, &launchNotificationKey);
+}
+
+- (void)setLaunchNotification:(NSDictionary *)aDictionary
+{
+    objc_setAssociatedObject(self, &launchNotificationKey, aDictionary, OBJC_ASSOCIATION_RETAIN_NONATOMIC);
+}
+
+- (NSNumber *)coldstart
+{
+    return objc_getAssociatedObject(self, &coldstartKey);
+}
+
+- (void)setColdstart:(NSNumber *)aNumber
+{
+    objc_setAssociatedObject(self, &coldstartKey, aNumber, OBJC_ASSOCIATION_RETAIN_NONATOMIC);
+}
+
+- (void)dealloc
+{
+    self.launchNotification = nil; // clear the association and release the object
+    self.coldstart = nil;
 }
 
 @end
